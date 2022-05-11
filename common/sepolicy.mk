b2382f45421825dad051ca833cc39434907cc62b
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-05-11 16:05:17 +0000

    sepolicy: Adapt for Octavi
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from OctaviOS
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -10,25 +10,25 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/awaken/sepolicy/common/public
+    device/octavi/sepolicy/common/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/awaken/sepolicy/common/private
+    device/octavi/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/awaken/sepolicy/common/dynamic \
-    device/awaken/sepolicy/common/system
+    device/octavi/sepolicy/common/dynamic \
+    device/octavi/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/awaken/sepolicy/common/dynamic \
-    device/awaken/sepolicy/common/vendor
+    device/octavi/sepolicy/common/dynamic \
+    device/octavi/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/awaken/sepolicy/legacy-common/sepolicy.mk
+-include device/octavi/sepolicy/legacy-common/sepolicy.mk
 
 # Include atv rules on atv product
 ifeq ($(PRODUCT_IS_ATV), true)
-include device/awaken/sepolicy/atv/sepolicy.mk
+include device/octavi/sepolicy/atv/sepolicy.mk
 endif
--- legacy-common/sepolicy.mk
+++ legacy-common/sepolicy.mk
@@ -1,9 +1,9 @@
 #
 # This policy configuration will be used by select legacy products that
-# inherit from Lineage
+# inherit from OctaviOS
 #
 
 ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1), true)
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/awaken/sepolicy/legacy-common/public/legacy-camera-hal1
+    device/octavi/sepolicy/legacy-common/public/legacy-camera-hal1
 endif
